1524ab2516b7ffd94b2d9ea131740a633f353cfe
xljiulang <366193849@qq.com>
2022-12-04 01:45:49 +0800

    add IConnectionFactory

---
+++ KestrelApp.Client/SocketConnectionFactoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace KestrelApp
+{
+    public static class SocketConnectionFactoryExtensions
+    {
+        /// <summary>
+        /// 注册SocketConnectionFactory为IConnectionFactory
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddSocketConnectionFactory(this IServiceCollection services)
+        {
+            const string typeName = "Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets.SocketConnectionFactory";
+            var factoryType = typeof(SocketTransportOptions).Assembly.GetType(typeName);
+            return factoryType == null
+                ? throw new NotSupportedException($"找不到类型{typeName}")
+                : services.AddSingleton(typeof(IConnectionFactory), factoryType);
+        }
+    }
+}
